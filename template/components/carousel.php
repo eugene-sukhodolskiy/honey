66af15cdb05b97ae0e8028046eca300594a665de
Eugene <goldmikki@gmail.com>
2021-03-22 08:02:16 +0200

    Personal slides for mobile screen
    Issue https://github.com/eugene-sukhodolskiy/honey/issues/24

--- template/components/carousel.php
+++ template/components/carousel.php
@@ -1,17 +1,27 @@
 <?
 	$slides = [];
+	$slides_mob = [];
 	for($i=1; $i<=10; $i++){
 		$slide = get_field("slide_{$i}");
-		if(!$slide){
-			break;
+		$slide_mob = get_field("slide_mob_{$i}");
+		if($slide){
+			$slides[] = $slide;
+		}else break;
+		if($slide_mob){
+			$slides_mob[] = $slide_mob;
 		}
-
-		$slides[] = $slide;
 	}
+
 ?>
 
 <div class="carousel">
 	<?php foreach ($slides as $i => $slide): ?>
+		<img src="<?= $slide ?>" class="slide-img" alt="">
+	<?php endforeach ?>
+</div>
+
+<div class="carousel mob">
+	<?php foreach ($slides_mob as $i => $slide): ?>
 		<img src="<?= $slide ?>" class="slide-img" alt="">
 	<?php endforeach ?>
 </div>
