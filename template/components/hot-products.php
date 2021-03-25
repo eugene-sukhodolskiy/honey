6399ea030fb991998f7292ab310737ecf8183ca2
Eugene <goldmikki@gmail.com>
2021-03-25 21:19:36 +0200

    Fix displaying
    Issue https://github.com/eugene-sukhodolskiy/honey/issues/31

--- template/components/hot-products.php
+++ template/components/hot-products.php
@@ -1,11 +1,11 @@
 <?
 	$hot_products = array_map(function($post){
 		return wc_get_product($post -> ID);
-	}, get_field('hot_products', $post -> ID));
+	}, get_field('hot_products', isset($post) ? $post -> ID : get_the_ID()));
 ?>
 
 <?= $this -> join('components/swipeable-product-line', [
-	'title' => get_field('block_title', $post -> ID),
+	'title' => get_field('block_title', isset($post) ? $post -> ID : get_the_ID()),
 	'additional_css_class' => 'recommended-products',
 	'products' => $hot_products
 ]) ?>
